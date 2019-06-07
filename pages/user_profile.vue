e7f49c5d630fc54fd9e396fadf95d3a095c2ffe4
kmaeder <kmaeder@kevin-maeder.de>
2019-06-07 07:10:34 +0200

    myUser query over graphql

--- pages/user_profile.vue
+++ pages/user_profile.vue
@@ -2,18 +2,24 @@
   <v-layout>
     <v-flex text-xs-center>
       <h1>User Profile</h1>
-      <p v-if="user">username: {{ user.username }}</p>
+      <p v-if="myUser">username: {{ myUser.username }} <br> name: {{ myUser.name }}</p>
       <p v-else>No user is signed in. Log in first!</p>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
+import myUser from '~/apollo/queries/myUser'
 export default {
   layout: 'inside',
   computed: {
     user() {
       return (this.$store.state.auth || {}).user || null
+    }
+  },
+  apollo: {
+    myUser: {
+      query: myUser
     }
   },
   created() {
